9fef7f3774b89637493117145b1de20b9417cc90
nosillard <nosillard@IUTCLINFB1014>
2023-11-17 16:58:41 +0100

    debog CF, pas fini

--- PHP/controleur/FrontControler.php
+++ PHP/controleur/FrontControler.php
@@ -6,11 +6,16 @@ namespace controleur;
 
 use Config\Validation;
 use modele\ModeleVisiteur;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 class FrontControler {
 
     public function __construct()
     {
+        global $twig;
         $actions = array(
             "Visiteur" => [
                 "seConnecter", "sInscrire", "accueil", "chercherArticle", "signalerArticle"
@@ -38,19 +43,53 @@ class FrontControler {
         $actions["Moderateur"] = array_merge($actions["Moderateur"], $actions["Redacteur"]);
         $actions["Admin"] = array_merge($actions["Admin"], $actions["Moderateur"]);
 
-        session_start();
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
         $modele = new ModeleVisiteur();
         $action = Validation::nettoyerString($_GET["action"] ?? "");
         $personne=$modele->estConnecte();
 
-        if (!$this->checkAccess($actions, $action, $personne)) {
+        /*
+       if (!$this->checkAccess($actions, $action, $personne)) {
             echo "<br>ERREUR : Accès refusé, vous ne pouvez pas faire cette action";
         }
+         */
+
+
+        $actionsConnection = array_merge(
+            $actions["Utilisateur"],
+            $actions["Redacteur"],
+            $actions["Moderateur"],
+            $actions["Admin"]
+        );
+
+        echo "Action from GET: " . $action;
+        //echo "Actions disponibles : ";
+        //print_r($actionsConnection);
+        echo "Action : " . $action;
+
+        if (in_array($action, $actionsConnection)) {
+            echo "1 er if passé";
+            if ($personne == null) {
+                echo $twig->render("Vue/connexion.php", []);
+                echo "<br>ERREUR : Vous n'êtes pas connecté, veuillez vous connecter pour accéder à cette fonctionnalité";
+                echo "2 eme if passé";
+            }
+            else{
+                echo "dans le else";
+                //echo $twig->render("test.html", []);
+                echo $twig->render("Vue/accueil.html", []);
+            }
+        }
+
     }
 
     private function checkAccess($actions, $action, $personne) {
+        global $twig;
         if ($personne == null) {
-            require("Vue/connexion.php");
+            //require("Vue/connexion.php");
+            $twig->render("Vue/connexion.php", []);
             echo "<br>ERREUR : Vous n'êtes pas connecté, veuillez vous connecter pour accéder à cette fonctionnalité";
             return false;
         } else {
@@ -73,15 +112,15 @@ class FrontControler {
                 new UtilisateurControleur();
                 break;
             /**case 'Redacteur':
-                new ControleurRedacteur();
-                break;
+            new ControleurRedacteur();
+            break;
             case 'Moderateur':
-                new ControleurModerateur();
-                break;
+            new ControleurModerateur();
+            break;
             case 'Admin':
-                new ControleurAdmin();
-                break;
-            */
+            new ControleurAdmin();
+            break;
+             */
             default:
                 new VisiteurControleur();
         }
--- PHP/index.php
+++ PHP/index.php
@@ -5,8 +5,16 @@ use controleur\FrontControler;
 require_once __DIR__ . '/config/config.php';
 require __DIR__ . '/vendor/autoload.php';
 
-$loader = new \Twig\Loader\FilesystemLoader('Vue');
-$twig = new Twig\Environment($loader, [ 'cache' => false, ]);
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/Vue');
+$twig = new \Twig\Environment($loader, ['cache' => false]);
 
-
-$cont = new FrontControler();
+//var_dump($loader->getPaths());
+try {
+    $cont = new FrontControler();
+} catch (\Twig\Error\LoaderError $e) {
+    echo 'Erreur de chargement du modèle : ' . $e->getMessage();
+} catch (\Twig\Error\RuntimeError $e) {
+    echo 'Erreur d\'exécution du modèle : ' . $e->getMessage();
+} catch (\Twig\Error\SyntaxError $e) {
+    echo 'Erreur de syntaxe du modèle : ' . $e->getMessage();
+}
