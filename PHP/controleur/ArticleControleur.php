4077c5567eff19e74e4632b44af2f71ff7b4fe85
nosillard <nosillard@IUTCLINFB1014>
2023-11-21 11:53:23 +0100

    evolution chargement articles

--- PHP/controleur/ArticleControleur.php
+++ PHP/controleur/ArticleControleur.php
@@ -1,20 +1,24 @@
 <?php
 
 namespace controleur;
+use dal\gateways\ArticleGateway; //pour $con
 
 //je suis vraiment pas sûr pour le controleur
 class ArticleControleur
 {
-    function __construct()
+    private $articleGateway;
+
+    function __construct($action, $id, $page)
     {
         global $twig;
-        try {
+        $this->articleGateway = new ArticleGateway($con);
 
-            switch () {
-                case afficherUnArticle:
-                    $this->afficherUnArticle();
+        try {
+            switch ($action) {
+                case 'afficherUnArticle':
+                    $this->afficherUnArticle( $id, $page);
                     break;
-                case afficherPageArticles:
+                case 'afficherPageArticles':
                     $this->afficherPageArticles();
                     break;
                 default:
@@ -25,31 +29,26 @@ class ArticleControleur
             echo $twig->render("Vue/error.php");
         }
     }
-    }
-    function afficherUnArticle(int $id){
-    //gateway va recher article $id
-        /*
-        $statement = $pdo->prepare('SELECT * FROM article WHERE id = :id');
-        $statement->execute(['id' => $id]);
-        $article = $statement->fetch(PDO::FETCH_ASSOC);
-        */
+
+    function afficherUnArticle(int $id, string $page) //page est la page d'affichage de l'article
+    {
         global $twig;
-        $article = array(
-        'titreParagraphe' => 'Titre du paragraphe',
-        'contenuParagraphe' => 'Contenu du paragraphe',
-        'imageTitre' => 'Titre de l\'image',
-        'imageChemin' => '/chemin/vers/image.jpg',
-        'imageAlt' => 'Texte alternatif de l\'image',
-        'videoTitre' => 'Titre de la vidéo',
-        'videoLink' => 'https://www.youtube.com/watch?v=xxxxxxxxxxx'
-        );
+        try {
+            $article = $this->articleGateway->getArticleById($id);
 
-        $blocs = array(
-        array('type' => 'paragraphe', 'data' => $article),
-        array('type' => 'image', 'data' => $article),
-        array('type' => 'video', 'data' => $article)
-        );
+            if ($article) {
+                $blocs = array(
+                    array('type' => 'paragraphe', 'data' => $article),
+                    array('type' => 'image', 'data' => $article),
+                    array('type' => 'video', 'data' => $article)
+                );
 
-        echo $twig->render('votre_page_twig.twig', ['TitreArticle' => 'Titre de votre article', 'blocs' => $blocs]);
+                echo $twig->render($page, ['TitreArticle' => $article['titreArticle'], 'blocs' => $blocs]);
+            } else {
+                echo $twig->render("Vue/error.php"); //article non trouvé
+            }
+        } catch (Exception $e) {
+            echo $twig->render("Vue/error.php");
+        }
     }
 }
