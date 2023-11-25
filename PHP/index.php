9fd260f0c5024a0859c4b05eb39a3dc3b92d38f9
Siwa12100 <jean.marcillac12@gmail.com>
2023-11-25 20:07:41 +0100

    travail sur la partie visuelle des différents blocs de contenu - Jean

--- PHP/index.php
+++ PHP/index.php
@@ -11,6 +11,6 @@ $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/Vue');
 $twig = new Twig\Environment($loader, [ 'cache' => false, 'debug' => true ]);
 
 session_start();
-//$cont = new FrontControler();
+$cont = new FrontControler();
 
-require("./brouillon/brouillonArticle.php");
+//require("./brouillon/brouillonArticle.php");
