3956f0738ad2c5045d8aebcb09fe3459951fea18
Siwa12100 <jean.marcillac12@gmail.com>
2023-11-25 17:47:58 +0100

    travail sur la partie visuelle des différents blocs de contenu - Jean

--- PHP/modele/StubArticles.php
+++ PHP/modele/StubArticles.php
@@ -34,9 +34,11 @@ use modele\ContenuParagraphe;
             $temp[] = new Article(3, "Titre du troisième Article", "Contenu du troisième article...\n seconde ligne... \n troisième ligne ", 7, date("d-m-Y"), "Siwa");
             $temp[] = new Article(4, "Titre du quatrième Article", "Contenu du quatrième article...\n\n\n seconde ligne...", 100, date("d-m-Y"), "Siwa");
             
-            foreach($temp as $article) {
-                $article -> remplirArticle($this -> chargerContenuParagraphe());
-            }
+             $temp[1] -> remplirArticle($this -> chargerContenuParagraphe()['article2']);
+             $temp[2] ->remplirArticle($this -> chargerContenuParagraphe()['article2']);
+             $temp[3] ->remplirArticle($this -> chargerContenuParagraphe()['article2']);
+
+             $temp[0] -> remplirArticle($this -> chargerContenuParagraphe()['article1']);
 
             $this -> lArticles = $temp;
         }
@@ -45,8 +47,17 @@ use modele\ContenuParagraphe;
 
             $temp = array();
 
-            $temp[] = new contenuParagraphe(1, "Premier paragraphe", "Contenu du premier article et tout et tout....");
-            $temp[] = new contenuParagraphe(2, "Titre du second paragraphe", "Contenu du second paragraphe....");
+            $temp['article2'] = new contenuParagraphe(1, "Premier paragraphe", "Contenu du premier article et tout et tout....");
+            $temp['article2'] = new contenuParagraphe(2, "Titre du second paragraphe", "Contenu du second paragraphe....");
+            
+            $temp['article1'] = new contenuParagraphe(3, "Le mystérieux cygne noir.", 
+                "Sous les traits d’un mystérieux cygne noir, un objet vidéo non identifié plane dans la galaxie 
+                médiatique. Ambiance Star Wars. Après une musique lancinante, sur fond noir et fumée grisâtre,
+                 un invité apparaît, et la voix d’un intervieweur-mystère situé hors champ l’invite à se
+                  présenter « succinctement ». C’est ainsi qu’on entre dans le monde de ThinkerView, 
+                  chaîne qui a commencé à diffuser en janvier 2013 sur YouTube. Ici, pas de publicité,
+                   pas de montage, pas d’effets de lumière. Le calme, peut-être pour annoncer la tempête.");
+            
             return $temp;
         }
 
