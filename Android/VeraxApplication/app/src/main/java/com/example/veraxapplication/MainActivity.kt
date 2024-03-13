50541396b65c62f5a80892dbe00b0d5c5be8ebe7
lolaborie <Louis.LABORIE@etu.uca.fr>
2024-03-13 15:20:23 +0100

    🚑🚑 BottomBar ajoutée + Fix Texte

--- Android/VeraxApplication/app/src/main/java/com/example/veraxapplication/MainActivity.kt
+++ Android/VeraxApplication/app/src/main/java/com/example/veraxapplication/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.veraxapplication
 
+import android.graphics.Paint.Align
 import android.os.Bundle
+import android.text.Layout
 import android.view.Menu
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -28,8 +31,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -45,6 +50,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -57,12 +63,12 @@ import androidx.compose.ui.unit.sp
 class MainActivity : ComponentActivity() {
     // un truc vite fait pour avoir un visi
     var article = listOf("Thinkerview", "thinkerview.jgp", "Thinkerview est une chaîne youtube d'interview-débat")
+    var theme = listOf("Economique","Culture","Politique","Faits divers")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // faut y mettre en column mais ca disparait ?? je regarderai demain, la dodo
-                TopBarVerax()
-                AffichageUnArticle(article = article)
+                TopBarVerax(theme = theme, article = article)
+            // allez sur la doc de Scaffold sur Android Dev et si vous comprenez comment on doit faire bien ouej
 
         }
     }
@@ -70,105 +76,96 @@ class MainActivity : ComponentActivity() {
 
 // Il faudrait mettre ca dans un fichier appart mais je connais plus les conventions ...
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview
 @Composable
-fun TopBarVerax(){
+fun TopBarVerax(theme : List<String>, article : List<String>) {
     var leMenu by remember {
         mutableStateOf(false)
     }
     Row(
         Modifier.background(Color.Cyan)
-    ){
-        Scaffold (
-            topBar = {CenterAlignedTopAppBar(
-                title = { Text(text = "Verax",
-                                style = TextStyle(fontSize = 35.sp),
-                                color = colorResource(R.color.red),
-                                textAlign = TextAlign.Center,
-                                /*backcolor = topAppBarColors(
+    ) {
+        Scaffold(
+            topBar = {
+                CenterAlignedTopAppBar(
+                    title = {
+                        Text(
+                            text = "Verax",
+                            style = TextStyle(fontSize = 35.sp),
+                            color = colorResource(R.color.red),
+                            textAlign = TextAlign.Center,
+                            /*backcolor = topAppBarColors(
                                     containerColor = MaterialTheme.colorScheme.primaryContainer),*/ //version recommandée par le prof
-                                modifier = Modifier.fillMaxWidth()
-                )},
-            navigationIcon = {
-                IconButton(onClick = { /* action() */ }) {
-                    Icon(
-                        imageVector =  Icons.Filled.ArrowBack,
-                        contentDescription = "Retour",
-                        Modifier.size(30.dp)
-                    )
-                }
-            },
-            actions = {
-                IconButton(onClick = { leMenu = !leMenu }) {
-                    Icon(
-                        imageVector = Icons.Filled.Menu,
-                        contentDescription = "Menu",
-                        Modifier.size(35.dp)
-                    )
-                }
-                DropdownMenu(expanded = leMenu, onDismissRequest = { leMenu = false },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(Color.hsl(0.08F, 1F, 0.96F))) {
-                    DropdownMenuItem(
-                        text = {
-                            Text("Economique",
-                                style = TextStyle(fontSize = 25.sp),
-                                modifier = Modifier
-                                    .padding(10.dp)
-                            )
-                        },
-                        onClick = { /* TODO */ }
-                    )
-                    DropdownMenuItem(
-                            text = {
-                                Text("Culture",
-                                    style = TextStyle(fontSize = 25.sp),
-                                    modifier = Modifier
-                                        .padding(10.dp)
-                                )
-                            },
-                    onClick = { /* TODO */ }
-                    )
-                    DropdownMenuItem(
-                        text = {
-                            Text("Politique",
-                                style = TextStyle(fontSize = 25.sp),
-                                modifier = Modifier
-                                    .padding(10.dp)
-                            )
-                        },
-                        onClick = { /* TODO */ }
-                    )
-                    DropdownMenuItem(
-                        text = {
-                            Text("Faits divers",
-                                style = TextStyle(fontSize = 25.sp),
-                                modifier = Modifier
-                                    .padding(10.dp)
-                            )
-                        },
-                        onClick = { /* TODO */ }
-                    )
-                    // au lieu de c/c 15 fois le truc on peut faire une boucle (a tester si ca marche) :
-                    /*
-                    themesArticle.forEachIndexed { theme, nom ->
-                        DropdownMenuItem(
-                            text = {
-                                Text(text=nom,
-                                style = TextStyle(fontSize = 25.sp),
-                                modifier = Modifier
-                                    .padding(10.dp)
-                                )
-                            },
-                            onClick = { /* faut un moyen d'appeler une methode diff pour chaque, ca doit etre faisable facilement */ }
+                            modifier = Modifier.fillMaxWidth()
                         )
-                    */
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = { /* action() */ }) {
+                            Icon(
+                                imageVector = Icons.Filled.ArrowBack,
+                                contentDescription = "Retour",
+                                Modifier.size(30.dp)
+                            )
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = { leMenu = !leMenu }) {
+                            Icon(
+                                imageVector = Icons.Filled.Menu,
+                                contentDescription = "Menu",
+                                Modifier.size(35.dp)
+                            )
+                        }
+                        DropdownMenu(
+                            expanded = leMenu, onDismissRequest = { leMenu = false },
+                            modifier = Modifier
+                                .background(Color.hsl(0.08F, 1F, 0.96F))
+                        ) {
+                            theme.sorted().forEach {
+                                DropdownMenuItem(
+                                    text = {
+                                        Text(
+                                            it,
+                                            style = TextStyle(fontSize = 25.sp),
+                                            modifier = Modifier
+                                                .padding(10.dp)
+                                        )
+                                    },
+                                    onClick = { /* faut un moyen d'appeler une methode diff pour chaque, ca doit etre faisable facilement */ }
+                                )
+                            }
+                        }
+                    }
+
+                )
+            },
+            bottomBar = {
+                // Faudrait pouvoir faire un flex sur les boutons parce que là ils sont juste côte à côte
+                BottomAppBar(containerColor = Color.Black, contentColor = Color.White) {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Home,
+                            contentDescription = "Home",
+                            Modifier.size(35.dp)
+                        )
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Person,
+                            contentDescription = "Account",
+                            Modifier.size(35.dp)
+                        )
+                    }
                 }
             }
-            )}
-        ){it
+        ) { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+            ) {
+                AffichageUnArticle(article = article)
 
+            }
         }
     }
 }
